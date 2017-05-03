dbd0bb75ac53032e00a1ef05eb332dfff68d2ea0
NaserDaliri <5022 29105 620 1401>
2017-05-03 10:28:33 +0430

    Initial Commit

---
+++ a2zServis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  a2zServis
+//
+//  Created by i Daliri on 5/3/17.
+//  Copyright © 2017 a2zApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

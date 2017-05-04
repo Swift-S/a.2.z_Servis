9f69dd20d6eb898533753110f0067cd0e17e2047
NaserDaliri <5022 29105 620 1401>
2017-05-04 13:32:38 +0430

    fix plus VC

--- a2zServis/PlassTabBarController.swift
+++ a2zServis/PlassTabBarController.swift
@@ -11,12 +11,28 @@ import UIKit
 class PlassTabBarController: UIViewController {
 
     @IBOutlet weak var plusIcon: UITabBarItem!
+    @IBOutlet weak var cleaningBttn: UIButton!
+    @IBOutlet weak var handymanBttn: UIButton!
+    @IBOutlet weak var plumbingBttn: UIButton!
+    @IBOutlet weak var electricalBttn: UIButton!
+    @IBOutlet weak var paintingBttn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
 //        plusIcon.title = "\u{e613}"
         plusIcon.badgeColor = UIColor.blue
+        self.cleaningBttn.setTitle("\u{e606}", for: .normal)
+        self.handymanBttn.setTitle("\u{e60a}", for: .normal)
+        self.plumbingBttn.setTitle("\u{e609}", for: .normal)
+        self.electricalBttn.setTitle("\u{e608}", for: .normal)
+        self.paintingBttn.setTitle("\u{e607}", for: .normal)
+//        self.cleaningBttn.layer.borderWidth = 2
+        self.cleaningBttn.layer.cornerRadius = 35
+        self.handymanBttn.layer.cornerRadius = 35
+        self.plumbingBttn.layer.cornerRadius = 35
+        self.electricalBttn.layer.cornerRadius = 35
+        self.paintingBttn.layer.cornerRadius = 35
     }
 
     override func didReceiveMemoryWarning() {
